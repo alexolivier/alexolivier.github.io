94a72b88768537ca0707c4c06be66519ddc703c1
Alex Olivier <alex@alexolivier.me>
2014-02-01 21:11:12 -0500

    added GA

--- src/contents/js/app.js
+++ src/contents/js/app.js
@@ -1,1 +1,19 @@
 //PHOTOS https://picasaweb.google.com/data/feed/api/user/100896582238361713598/albumid/5973264711378523937
+
+
+
+//GA
+(function(i, s, o, g, r, a, m) {
+  i['GoogleAnalyticsObject'] = r;
+  i[r] = i[r] || function() {
+    (i[r].q = i[r].q || []).push(arguments);
+  }, i[r].l = 1 * new Date();
+  a = s.createElement(o),
+  m = s.getElementsByTagName(o)[0];
+  a.async = 1;
+  a.src = g;
+  m.parentNode.insertBefore(a, m)
+})(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+ga('create', 'UA-41058864-1', 'alexolivier.me');
+ga('send', 'pageview');
